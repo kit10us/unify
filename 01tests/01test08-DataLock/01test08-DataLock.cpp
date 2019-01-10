ebb7ec954fa67b4a81a1c438d1c1fd952e53f19b
Stacy Smith <stacy@kit10.us>
2019-01-10 07:59:46 -0600

    Added DataLock unit tests.

---
+++ 01tests/01test08-DataLock/01test08-DataLock.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2002 - 2018, Evil Quail LLC
+// All Rights Reserved
+
+/// <summary>
+/// Test DataLock.
+/// </summary>
+
+#include <unify/DataLock.h>
+#include <unify/String.h>
+#include <unify/test/Suite.h>
+
+int main( int argc, char ** argv )
+{
+	using namespace unify;
+	using namespace test;
+	using namespace string;
+
+	Suite suite;
+	suite.BeginSuite( "DataLock" );
+	{
+		suite.BeginCase( "FromString" );
+		suite.Assert( "from \"None\"", DataLock::None == DataLock::FromString( "None" ) );
+		suite.Assert( "from \"Readonly\"", DataLock::Readonly == DataLock::FromString( "Readonly" ) );
+		suite.Assert( "from \"Writeonly\"", DataLock::Writeonly == DataLock::FromString( "Writeonly" ) );
+		suite.Assert( "from \"Readonly\"", DataLock::ReadWrite == DataLock::FromString( "ReadWrite" ) );
+		suite.EndCase();
+
+		suite.BeginCase( "ToString" );
+		suite.Assert( "from \"None\"", "None" == DataLock::ToString( DataLock::None ) );
+		suite.Assert( "from \"Readonly\"", "Readonly" == DataLock::ToString( DataLock::Readonly ) );
+		suite.Assert( "from \"Writeonly\"", "Writeonly" == DataLock::ToString( DataLock::Writeonly ) );
+		suite.Assert( "from \"Readonly\"", "ReadWrite" == DataLock::ToString( DataLock::ReadWrite ) );
+		suite.EndCase();
+	}
+	suite.EndSuite();
+	return 0;
+}
