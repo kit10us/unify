3a701d3d4d85c372521041eeef9a0f7e428d096d
Stacy Smith <stacyqsmity@gmail.com>
2021-07-07 09:52:16 -0500

    Update copyright to new company LLC.

--- unify/Any.h
+++ unify/Any.h
@@ -1,5 +1,24 @@
-// Copyright (c) 2002 - 2019, Kit10 Studios LLC
-// All Rights Reserved
+/*
+ * Unify Library
+ * https://github.com/kit10us/unify
+ * Copyright (c) 2002, Kit10 Studios LLC
+ *
+ * This file is part of Unify Library (a.k.a. Unify)
+ *
+ * Unify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Unify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Unify.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 
 /// NOTICE: The contents of this header were pulled from the Boost library, modified to remove usage of Boost, and replace that usage with C++11.
 ///         This was done as Any is the last remaining feature from boost, not provided via C++11, that I like to use, and I do not want to have to
--- unify/BSphere.h
+++ unify/BSphere.h
@@ -1,6 +1,24 @@
-/* Copyright (c) 2002 - 2019, Kit10 Studios LLC
- * All Rights Reserved
+/*
+ * Unify Library
+ * https://github.com/kit10us/unify
+ * Copyright (c) 2002, Kit10 Studios LLC
+ *
+ * This file is part of Unify Library (a.k.a. Unify)
+ *
+ * Unify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Unify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Unify.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 
 #pragma once
 
